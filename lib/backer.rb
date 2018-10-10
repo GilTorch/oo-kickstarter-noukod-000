06dc3c1a80705b7b81f95b57e237c123e72b9c4c
Gilbert Torchon <gilbert@noukod.com>
2018-10-10 15:47:51 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,19 @@
 class Backer
 
-  attr_accessor :name,:backed_projects
+  attr_accessor :name,:backed_projects,:backers
 
   def initialize(name)
     @name=name
     @backed_projects=[]
+    @backers=[]
   end
 
   def back_project(project)
     @backed_projects << project
+  end
+
+  def add_backer(backer)
+    @backer << backer
   end
 
 end
